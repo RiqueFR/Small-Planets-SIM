e167aa9a81ace6ad01118a9924ea9b6a59a7c381
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2022-02-07 23:06:06 -0300

    Small fix

--- main.js
+++ main.js
@@ -75,13 +75,12 @@ class Planet {
 				// M => Kg
 				// d => m
 				// a => m/s2
-				let mass = planet.mass;
 				let distance_vet = createVector(planet.x, planet.y).sub(createVector(this.x, this.y));
 				let distance = distance_vet.mag(); //distance value come in pixels
 				let distanceMeters = distance*(1/pixelScale)*1000;
 
 				// acceleration => m/s2
-				let acceleration = gravitation * mass / (distanceMeters*distanceMeters);
+				let acceleration = gravitation * planet.mass / (distanceMeters*distanceMeters);
 
 				// transforming it in a vector
 				let acceleration_vet = distance_vet.normalize().setMag(acceleration);
@@ -107,21 +106,21 @@ function setup() {
 	buttonClear = createButton("Clear");
 	buttonClear.mousePressed(clearPlanets);
 
-	planets.push(new Planet(100, 100, 30, (5*10**24), 0, 0, 255));
-	planets.push(new Planet(500, 500, 20, (5*10**24)));
-	planets.push(new Planet(500, 0, 100, (5*10**24)));
-	planets.push(new Planet(400, 200, 60, (5*10**24)));
+	//planets.push(new Planet(100, 100, 30, (5*10**24), 0, 0, 255));
+	//planets.push(new Planet(500, 500, 20, (5*10**24)));
+	//planets.push(new Planet(500, 0, 100, (5*10**24)));
+	//planets.push(new Planet(400, 200, 60, (5*10**24)));
 
-	//let moonHeight = 0;
+	let moonHeight = 300;
 	//planets.push(new Planet(width/2, -70 + height/2, 6.5, (5*10**24)));
-	//planets.push(new Planet(width/2, 70 + height/2, 6.5, (5*10**24)));
-	//planets.push(new Planet(width/2, moonHeight + height/2, 1.75, (7.36*10**22)));
+	planets.push(new Planet(width/2, height/2, 6.5, (5*10**24)));
+	planets.push(new Planet(width/2, moonHeight + height/2, 1.75, (7.36*10**22)));
 
 	background(0);
 	for(let planet of planets) {
 		planet.draw();
 	}
-	//planets[1].accelerate(3670,0);
+	planets[1].accelerate(3670,0);
 	timeVel = 60000;
 
 }
